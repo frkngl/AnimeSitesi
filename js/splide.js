a6041344c7a86c7cf276d668de59956ac0a507f3
frkngl <frknglww@gmail.com>
2024-05-05 11:54:54 +0300

    ANASAYFADA KATEGORİLER KISIMLARI TAMAMLANDI VE RESPONSİVE HALİNE GETİRİLDİ

--- js/splide.js
+++ js/splide.js
@@ -1,20 +1,201 @@
-var splide = new Splide( '.splide', {
-    perPage: 6,
-    type   : 'loop',
-    gap    : '2rem',
-    padding: '5rem',
-    breakpoints: {
-      640: {
-        perPage: 2,
-        gap    : '.7rem',
-        height : '6rem',
-      },
-      480: {
-        perPage: 1,
-        gap    : '.7rem',
-        height : '6rem',
-      },
+new Splide('#splide1', {
+  type: 'slide',
+  perPage: 6,
+  gap: '2em',
+  padding: '4rem',
+  breakpoints: {
+    1500: {
+      perPage: 5,
+      gap: '2rem',
+      padding: '3rem',
     },
-  } );
-  
-  splide.mount();
+    1200: {
+      perPage: 4,
+      gap: '2rem',
+      padding: '2rem',
+    },
+    900: {
+      perPage: 3,
+      gap: '2rem',
+      padding: '3rem',
+    },
+    700: {
+      perPage: 3,
+      gap: '1rem',
+      padding: '3rem',
+    },
+    600: {
+      perPage: 2,
+      gap: '1rem',
+      padding: '3rem',
+    },
+    400: {
+      perPage: 2,
+      gap: '1rem',
+      padding: '2.5rem',
+    },
+  },
+}).mount();
+
+new Splide('#splide2', {
+  type: 'slide',
+  perPage: 6,
+  gap: '2em',
+  padding: '4rem',
+  breakpoints: {
+    1500: {
+      perPage: 5,
+      gap: '2rem',
+      padding: '3rem',
+    },
+    1200: {
+      perPage: 4,
+      gap: '2rem',
+      padding: '2rem',
+    },
+    900: {
+      perPage: 3,
+      gap: '2rem',
+      padding: '3rem',
+    },
+    700: {
+      perPage: 3,
+      gap: '1rem',
+      padding: '3rem',
+    },
+    600: {
+      perPage: 2,
+      gap: '1rem',
+      padding: '3rem',
+    },
+    400: {
+      perPage: 2,
+      gap: '1rem',
+      padding: '2.5rem',
+    },
+  },
+}).mount();
+
+new Splide('#splide3', {
+  type: 'slide',
+  perPage: 6,
+  gap: '2em',
+  padding: '4rem',
+  breakpoints: {
+    1500: {
+      perPage: 5,
+      gap: '2rem',
+      padding: '3rem',
+    },
+    1200: {
+      perPage: 4,
+      gap: '2rem',
+      padding: '2rem',
+    },
+    900: {
+      perPage: 3,
+      gap: '2rem',
+      padding: '3rem',
+    },
+    700: {
+      perPage: 3,
+      gap: '1rem',
+      padding: '3rem',
+    },
+    600: {
+      perPage: 2,
+      gap: '1rem',
+      padding: '3rem',
+    },
+    400: {
+      perPage: 2,
+      gap: '1rem',
+      padding: '2.5rem',
+    },
+  },
+}).mount();
+
+
+new Splide('#splide4', {
+  type: 'slide',
+  perPage: 6,
+  gap: '2em',
+  padding: '4rem',
+  breakpoints: {
+    1500: {
+      perPage: 5,
+      gap: '2rem',
+      padding: '3rem',
+    },
+    1200: {
+      perPage: 4,
+      gap: '2rem',
+      padding: '2rem',
+    },
+    900: {
+      perPage: 3,
+      gap: '2rem',
+      padding: '3rem',
+    },
+    700: {
+      perPage: 3,
+      gap: '1rem',
+      padding: '3rem',
+    },
+    600: {
+      perPage: 2,
+      gap: '1rem',
+      padding: '3rem',
+    },
+    400: {
+      perPage: 2,
+      gap: '1rem',
+      padding: '2.5rem',
+    },
+  },
+}).mount();
+
+new Splide('#splide5', {
+  type: 'slide',
+  perPage: 6,
+  gap: '2em',
+  padding: '4rem',
+  breakpoints: {
+    1500: {
+      perPage: 5,
+      gap: '2rem',
+      padding: '3rem',
+    },
+    1200: {
+      perPage: 4,
+      gap: '2rem',
+      padding: '2rem',
+    },
+    900: {
+      perPage: 3,
+      gap: '2rem',
+      padding: '3rem',
+    },
+    700: {
+      perPage: 3,
+      gap: '1rem',
+      padding: '3rem',
+    },
+    600: {
+      perPage: 2,
+      gap: '1rem',
+      padding: '3rem',
+    },
+    400: {
+      perPage: 2,
+      gap: '1rem',
+      padding: '2.5rem',
+    },
+  },
+}).mount();
+
+
+
+
+
+
