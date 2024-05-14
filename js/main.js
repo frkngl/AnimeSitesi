b5619a7724ea8264c80532a06580bfb22b9abe30
Furkan Gül <frknglww@gmail.com>
2024-05-14 18:01:38 +0300

    ANİME DETAYLAR KISMI YAPILDI VE BİR KAÇ SAYFADA DÜZENLEMELER YAPILDI.

--- js/main.js
+++ js/main.js
@@ -54,23 +54,22 @@ var swiper = new Swiper(".mySwiper", {
 /*KATEGORİLER DÖNGÜYE ALAN EDİTLER*/
 
 const video = document.querySelectorAll(".splide__slide .video");
- video.forEach(video => {
-   video.onmouseenter = () => {
-     video.play();
-     video.volume= 0.2;
-     video.classList.add("active");
-   };
+video.forEach(video => {
+    video.onmouseenter = () => {
+        video.play();
+        video.volume = 0.02;
+        video.classList.add("active");
+    };
 
-   video.onmouseleave = () => {
-     video.pause();
-     video.classList.remove("active");
-  };
- });
+    video.onmouseleave = () => {
+        video.pause();
+        video.classList.remove("active");
+    };
+});
 
 
-
- //Aramalarda büyük küçük harf duyarlılığı için
-jQuery.expr[':'].contains = function(a, i, m) {
+//Aramalarda büyük küçük harf duyarlılığı için
+jQuery.expr[':'].contains = function (a, i, m) {
     return jQuery(a).text().toUpperCase()
         .indexOf(m[3].toUpperCase()) >= 0;
 };
@@ -78,21 +77,21 @@ jQuery.expr[':'].contains = function(a, i, m) {
 $(document).ready(function () {
 
     // keyup ile inputa herhangi bir değer girilince fonksiyonu tetikliyoruz
-    $("#searchTags").keyup(function(){
+    $("#searchTags").keyup(function () {
 
         // inputa yazılan değeri alıyoruz
         var value = $("#searchTags").val();
 
         // eğer input içinde değer yoksa yani boşsa tüm menüyü çıkartıyoruz
-        if(value.length==0){
-		
+        if (value.length == 0) {
+
             $("#menuFull li").show();
 
-		// arama yapılmışsa ilk olarak tüm menüyü gizliyoruz ve girilen değer ile eşleşen kısmı çıkarıyoruz
-        }else{
+            // arama yapılmışsa ilk olarak tüm menüyü gizliyoruz ve girilen değer ile eşleşen kısmı çıkarıyoruz
+        } else {
 
             $("#menuFull li").hide();
-            $("#menuFull li:contains("+value+")").show();
+            $("#menuFull li:contains(" + value + ")").show();
 
         }
 
